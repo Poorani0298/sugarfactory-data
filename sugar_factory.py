66fce3e628c2191caabac90415960e27215a8c3e
Poorani0298 <pornajoan@gmail.com>
2020-09-26 21:25:36 +0530

    added in output.csv

--- sugar_factory.py
+++ sugar_factory.py
@@ -2,25 +2,46 @@ from lxml import html
 import cssselect 
 import requests
 import csv
+# files=["./sugarfactoryindia/Sugar Factories in India-1 - Anekant Prakashan.html","./sugarfactoryindia/Sugar Factories in India-2 - Anekant Prakashan.html","./sugarfactoryindia/Sugar Factories in India-3 - Anekant Prakashan.html","./sugarfactoryindia/Sugar Factories in India-4 - Anekant Prakashan.html","./sugarfactoryindia/Sugar Factories in India-5 - Anekant Prakashan.html"]
+files=list(range(1,89))
+with open('output.csv', 'w', newline='') as file:
+		writer = csv.writer(file)
+		writer.writerow(["Company Name","Plant name","Address","Contact Details"])
+		for file_number in files:
+			content = open("./sugarfactoryindia/Sugar Factories in India-{} - Anekant Prakashan.html".format(file_number)).read()
+			tree = html.fromstring(content)    
+			company_names=tree.cssselect('div.search-item')
+			for company_name in company_names:
+				writer.writerow([
+				company_name.cssselect('h3')[0].cssselect('a')[0].text,
+				company_name.cssselect('div div p')[0].cssselect('span')[0].text,
+				company_name.cssselect('div div p')[1].cssselect('span')[0].text,
+				company_name.cssselect('div div p')[2].cssselect('span')[0].text
+				])
 
-#import glob
-file = "./sugarfactory/www.anekantprakashan.com/sugar-factories-in-amroha/district"
 
+# urls=[
+# 			'https://www.anekantprakashan.com/sugar-factories-in-ahmednagar/district',
+#       'https://www.anekantprakashan.com/sugar-factories-in-akola/district',
+#       'https://www.anekantprakashan.com/sugar-factories-in-aligarh/district',
+#       'https://www.anekantprakashan.com/sugar-factories-in-ambala/district']
+# for url in urls:
+# 	page=requests.get(url)
 
-content = open(file).read()
-
-tree = html.fromstring(content)    
-company_names=tree.cssselect('div.search-item')
-with open('lists.csv', 'w', newline='') as file:
-	writer = csv.writer(file)
-	writer.writerow(["Company Name","Plant name","Address","Contact Details"])
-	for company_name in company_names:
-		writer.writerow([
-		company_name.cssselect('h3')[0].cssselect('a')[0].text,
-		company_name.cssselect('div div p')[0].cssselect('span')[0].text,
-		company_name.cssselect('div div p')[1].cssselect('span')[0].text,
-		company_name.cssselect('div div p')[2].cssselect('span')[0].text
-		])
+# file = "./sugarfactory/www.anekantprakashan.com/sugar-factories/india"
+# content = open(file).read()
+# tree = html.fromstring(content)    
+# company_names=tree.cssselect('div.search-item')
+# with open('output.csv', 'w', newline='') as file:
+# 	writer = csv.writer(file)
+# 	writer.writerow(["Company Name","Plant name","Address","Contact Details"])
+# 	for company_name in company_names:
+# 		writer.writerow([
+# 		company_name.cssselect('h3')[0].cssselect('a')[0].text,
+# 		company_name.cssselect('div div p')[0].cssselect('span')[0].text,
+# 		company_name.cssselect('div div p')[1].cssselect('span')[0].text,
+# 		company_name.cssselect('div div p')[2].cssselect('span')[0].text
+# 		])
 
 			
 		
@@ -36,6 +57,17 @@ with open('lists.csv', 'w', newline='') as file:
 
 
 
+# url([
+# 	file.home/poorani/python_script/sugarfactory/
+# 	www.anekantprakashan.com/sugar-factories-in-ahmednagar/
+# 	district,
+# 	file.home/poorani/python_script/sugarfactory/
+# 	www.anekantprakashan.com/sugar-factories-in-akola/district,
+# 	file.home/poorani/python_script/sugarfactory/
+# 	www.anekantprakashan.com/sugar-factories-in-aligarh/district,
+# 	file.home/poorani/python_script/sugarfactory/
+# 	www.anekantprakashan.com/sugar-factories-in-ambala/district
+# 		])
 
 
 
